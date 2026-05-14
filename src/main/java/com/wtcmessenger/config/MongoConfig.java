ed2b02d256f2538d5b2f04f879fb9489e963636c
Rodrigo Belarmino <rb.belarmino@gmail.com>
2026-05-14 15:42:17 -0300

    feat: switch to custom MongoConfig and dynamic URI configuration with Docker Compose environment defaults

--- src/main/java/com/wtcmessenger/config/MongoConfig.java
+++ src/main/java/com/wtcmessenger/config/MongoConfig.java
@@ -1,11 +1,44 @@
 package com.wtcmessenger.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "com.wtcmessenger.repository")
 @Configuration
-public class MongoConfig {
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${mongodb.uri}")
+    private String mongoUri;
+
+    @Override
+    protected String getDatabaseName() {
+        // Extrai o nome do banco da URI (ex: /wtc_messenger?appName=...)
+        ConnectionString cs = new ConnectionString(mongoUri);
+        String database = cs.getDatabase();
+        return (database != null && !database.isBlank()) ? database : "wtc_messenger";
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(settings);
+    }
+
+    @Override
+    protected boolean autoIndexCreation() {
+        return true;
+    }
 }
