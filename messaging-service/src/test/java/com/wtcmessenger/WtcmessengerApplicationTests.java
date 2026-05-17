a75319e06cb795f963217a49314a86744efd81c9
Rodrigo Belarmino <rb.belarmino@gmail.com>
2026-05-16 22:54:38 -0300

    chore: configure test environment properties and add lombok version to pom.xml

--- messaging-service/src/test/java/com/wtcmessenger/WtcmessengerApplicationTests.java
+++ messaging-service/src/test/java/com/wtcmessenger/WtcmessengerApplicationTests.java
@@ -3,7 +3,11 @@ package com.wtcmessenger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"JWT_SECRET=dummy_secret_for_tests",
+		"MONGODB_URI=mongodb://localhost:27017/test_db",
+		"KAFKA_BOOTSTRAP_SERVERS=localhost:9092"
+})
 class WtcmessengerApplicationTests {
 
 	@Test
